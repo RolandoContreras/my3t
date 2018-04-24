a42916c5dacd11d3abdb18522da5ba46a24e3ce8
unknown <RolandoContreras>
2018-04-24 01:01:29 -0500

    changes text on products

--- application/views/backoffice/b_product.php
+++ application/views/backoffice/b_product.php
@@ -46,12 +46,13 @@
                                 <div class="col-md-9"> 
 					<div class="panel panel-default">
 						<div class="panel-heading clearfix"> 
-                                                    <div class="panel-title"><b>PLATAFORMA GLOBAL PASS</b></div> 
+                                                    <div class="panel-title"><b>GLOBAL PASS</b></div> 
 						</div> 
 						<!-- panel body --> 
 						<div class="panel-body"> 
-                                                    <p>Te otorgamos un estilo de vida distinto, podrás viajar a lugares paradisiacos con nosotros siendo parte de 3T LIFESTYLE. 	
-                                                    Tendrás 3 plataformas GLOBAL PASS PERÚ, INTERNACIONAL Y ELITE que te permitirán tener hasta el 70% de descuentos en: cientos de establecimiento dentro de Perú, hoteles, restaurantes, autos, cruceros, vuelos y mucho más.<br/>Puedes acumular puntos boomerang o millas para canjearlos por viajes, cruceros, vuelos o cualquier servicio de la plataforma todo pagado simplemente por recomendar la plataforma y que tus recomendados compren a través tuyo. 
+                                                    <p>Te otorgamos un estilo de vida distinto, podrás viajar a lugares paradisiacos con nosotros siendo parte de <b>3T CLUB</b>.<br/> 	
+                                                        Tendrás 3 plataformas <b>GLOBAL PASS PERÚ, INTERNACIONAL Y ELITE</b> que te permitirán tener hasta el 70% de descuentos en cientos de establecimiento dentro de Perú y a nivel internacional como en hoteles, restaurantes, autos, cruceros, vuelos y mucho más.<br/>
+                                                        Puedes acumular créditos de recompensa a través de nuestro sistema boomerang rewards para canjearlos por viajes, cruceros, vuelos o cualquier servicio de la plataforma todo pagado simplemente por recomendar 3T CLUB y GLOBAL PASS y que tus recomendados compren a través tuyo. 
                                                     </p><br/>
                                                     <a href="<?php echo site_url().'backoffice/productos/globalpass';?>"><button class="btn btn-success btn-block" type="button">VER PLATAFORMAS</button></a>
 						</div> 
@@ -78,7 +79,7 @@
 						</div> 
 						<!-- panel body --> 
 						<div class="panel-body"> 
-                                                    <p>Descubre el mecanismo de tu “mente” con el coaching transformacional basado en experiencias vivenciales de ciencia práctica que te ayudarán a recuperar el poder de tu vida a través de la autoconfianza. Un sistema probado que esta llevando al éxitos a cientos de persona.<br/>Tendrás temas como:
+                                                    <p>Descubre el mecanismo de tu “mente” con el coaching transformacional basado en experiencias vivenciales de ciencia práctica que te ayudarán a recuperar el poder de tu vida a través de la autoconfianza. Un sistema probado que esta llevando al éxitos a cientos de personas.<br/>Tendrás temas como:
 ¿Cómo eliminar la basura mental?, entrena tu enfoque para ver oportunidades, ¿Cómo crear relaciones y la vida social que siempre quisiste?, ¿Cómo superar los miedos?, convertir espirales de muerte a vida y mucho más.
 </p><br/>
                                                     <a href="http://sistemadenuevepasos.com/miembros/" target="_blank"><button disabled="" class="btn btn-success btn-block" type="button">IR AL SISTEMA</button></a>
@@ -105,7 +106,7 @@
 						</div> 
 						<!-- panel body --> 
 						<div class="panel-body"> 
-                                                    <p>Profesionales altamente calificados en el manejo de fondos de inversión con experiencias en compañías prestigiosas en Latinoamérica y Europa forman el staff de 3T ACADEMY.
+                                                    <p>Profesionales altamente calificados en el manejo de fondos de inversión con experiencias en compañías prestigiosas en Latinoamérica y Europa forman el staff de <b>3T ACADEMY</b>.
                                                         Nuestros profesionales son referentes y voceros en distintos medios de comunicación como américa noticias, canal N, diario gestión, el comercio, Perú 21, RPP Noticias entre otras.<br/>A la vez con un Track Record de hasta 20% de ganancia mensual en operaciones de manejo de fondos.
                                                         <br/>Fundamentos básicos, entrenamientos personalizados, señales, estrategias y noticias del día tendrás en nuestra academia.</p><br/>
                                                     <a href="<?php echo site_url().'backoffice/productos/academy';?>"><button disabled="" class="btn btn-success btn-block" type="button">IR A 3T ACADEMY</button></a>
