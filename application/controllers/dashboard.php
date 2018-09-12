fad0ab052f5fa849a78cc9344aa0e53cf999edf2
software.contreras@gmail.com <software.contreras@gmail.com>
2018-09-12 17:17:45 -0500

    add permisson on dashboard

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -11,6 +11,21 @@ class Dashboard extends CI_Controller {
     }
     
     public function validate(){
+        
+        if (isset($_SERVER['HTTP_ORIGIN'])) {  
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");  
+            header('Access-Control-Allow-Credentials: true');  
+            header('Access-Control-Max-Age: 86400');   
+        }  
+
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {  
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))  
+                header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");  
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))  
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");  
+        }
         if($this->input->is_ajax_request()){  
             
             $this->form_validation->set_rules('email','email',"required|trim|valid_email|callback_validar_user");
